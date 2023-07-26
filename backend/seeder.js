d127613a53317e726e88069a099504454a45a08d
Zaidi <zaidiahsan.r@gmail.com>
2023-07-26 16:50:48 -0500

    seeder data

---
+++ backend/seeder.js
@@ -0,0 +1,60 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import colors from 'colors';
+import users from './data/users.js';
+import products from './data/products.js';
+import User from './models/userModel.js';
+import Product from './models/productModel.js';
+import Order from './models/orderModel.js';
+import connectDB from './config/db.js';
+
+dotenv.config();
+
+connectDB();
+
+const importData = async () => {
+    try {
+        //before importing delete everything to avoid duplication
+        await Order.deleteMany();
+        await Product.deleteMany();
+        await User.deleteMany();
+
+        //fill users from user data into the variable createdUsers
+        const createdUsers = await User.insertMany(users);
+
+        //get the admin user bc only they can create products
+        const adminUser = createdUsers[0]._id;
+
+        const sampleProducts = products.map((product) => {
+            return { ...product, user: adminUser };
+        });
+
+        await Product.insertMany(sampleProducts);
+
+        console.log('Data Imported!'.green.inverse);
+        process.exit();
+    } catch (error) {
+        console.log(`${error}`.red.inverse);
+        process.exit(1);
+    }
+};
+
+const destroyData = async () => {
+    try {
+        await Order.deleteMany();
+        await Product.deleteMany();
+        await User.deleteMany();
+
+        console.log('Data Destroyed!'.red.inverse);
+        process.exit();
+    } catch (error) {
+        console.log(`${error}`.red.inverse);
+        process.exit(1);
+    }
+};
+
+if (process.argv[2] === '-d') {
+    destroyData();
+} else {
+    importData();
+}
