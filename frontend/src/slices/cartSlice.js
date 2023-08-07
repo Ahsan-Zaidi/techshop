160e8692523c104ba6ad091ece2766dbf16c8ef5
Zaidi <zaidiahsan.r@gmail.com>
2023-08-07 12:46:59 -0500

    checkout steps

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -3,7 +3,7 @@ import { updateCart } from "../utils/cartUtils";
 
 //setting the initial state variable
 /**check local storage to find any cart items if there is an item there we parse whatever is there else store that item in an empty array called 'cart' */
-const initialState = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : {cartItems: []};
+const initialState = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : {cartItems: [], shippingAddress: {}, paymentMethod: 'PayPal'};
 
 const cartSlice = createSlice({
     name: 'cart',
@@ -29,9 +29,13 @@ const cartSlice = createSlice({
 
             return updateCart(state);
         },
+        saveShippingAddress: (state, action) => {
+            state.shippingAddress = action.payload;
+            return updateCart(state);
+        }
     },
 });
 
-export const { addToCart, removeFromCart } = cartSlice.actions;
+export const { addToCart, removeFromCart, saveShippingAddress } = cartSlice.actions;
 
 export default cartSlice.reducer;
