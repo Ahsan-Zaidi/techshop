2ecc16a1bf734891378edce73038118df7000c3f
Zaidi <zaidiahsan.r@gmail.com>
2023-08-17 12:13:29 -0500

    filter so only images are uploaded

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -14,26 +14,33 @@ const storage = multer.diskStorage({
 });
 
 //function to make sure only image files are uploaded
-function checkFileType(file, cb) {
-    const filetypes = /jpg|jpeg|png/;
-    const extname = filetypes.text(path.extname(file.originalname).toLowerCase());
-    const mimetype = filetypes.test(file.mimetype);
+function fileFilter(req, file, cb) {
+    const filetypes = /jpe?g|png|webp/;
+    const mimetypes = /image\/jpe?g|image\/png|image\/webp/;
+  
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = mimetypes.test(file.mimetype);
+  
     if (extname && mimetype) {
-        return cb(null, true);
+      cb(null, true);
     } else {
-        cb("Images only!");
+      cb(new Error('Images only!'), false);
     }
-};
-
-//using multer to actually upload the image files
-const upload = multer({
-    storage,
-});
-
-router.post('/', upload.single('image'), (req, res) => {
-    res.send({
-        message: 'Image Uploaded',
-        image: `/${req.file.path}`
+  }
+  
+  const upload = multer({ storage, fileFilter });
+  const uploadSingleImage = upload.single('image');
+  
+  router.post('/', (req, res) => {
+    uploadSingleImage(req, res, function (err) {
+      if (err) {
+        res.status(400).send({ message: err.message });
+      }
+  
+      res.status(200).send({
+        message: 'Image uploaded successfully',
+        image: `/${req.file.path}`,
+      });
     });
 });
 
