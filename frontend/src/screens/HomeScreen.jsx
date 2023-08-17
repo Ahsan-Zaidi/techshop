36d2a4f5498bf4a0ca7c89ff725a7c36c886c5a9
Zaidi <zaidiahsan.r@gmail.com>
2023-08-16 19:11:03 -0500

    page number pagination for keyword filter

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -7,8 +7,8 @@ import Paginate from '../components/Paginate';
 import { useGetProductsQuery } from '../slices/productsApiSlice';
 
 const HomeScreen = () => {
-    const { pageNumber } = useParams();
-    const { data, isLoading, error } = useGetProductsQuery({ pageNumber });
+    const { keyword, pageNumber } = useParams();
+    const { data, isLoading, error } = useGetProductsQuery({ keyword, pageNumber });
 
     return (
       <>
@@ -31,6 +31,7 @@ const HomeScreen = () => {
             <Paginate 
               pages={data.pages}
               page={data.page}
+              keyword={keyword ? keyword : ''}
             />
           </>
         )}
